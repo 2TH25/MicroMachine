09d8a1a5c863ae2d599b77f52534f64bc4203b7d
2TH25 <mattheo.girardclos@gmail.com>
2024-05-06 01:30:43 +0200

    rien changer

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ let clic = { x: 0, y: 0 };
 // const nombreObstacleRebondissement=10;
 // const deltaT=0;
 
+
+
 function init() {
     context = document.getElementById("cvs").getContext("2d");
     context.width = document.getElementById("cvs").width;
     context.height = document.getElementById("cvs").height;
-    
 
 
     boucleDeJeu();
@@ -38,7 +39,9 @@ function update(dt) {
 
 function render() {
     context.clearRect(0, 0, context.width, context.height);
-
+    
     context.fillStyle = "red"
     context.fillRect(0,0,50,50);
+
+    
 }
