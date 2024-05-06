2d9282de92c4ad1f7d887120b8f87e252d0c982c
Alban Pichot <albanpcht@gmail.com>
2024-05-06 13:57:07 +0200

    sauvegarde avant fusion

--- vehicule.js
+++ vehicule.js
@@ -1,15 +1,20 @@
+
 class Vehicule {
+    
 
     constructor() {
+        this.position={x:0,y:0};
+        couleur; //entier
+        vitesse=0; //décimal
+        nbtour=0; //entier
+        checkpoint=0;//entier
 
     }
 
     init() {
         yo
-        
     }
 }
-
 
 
 
