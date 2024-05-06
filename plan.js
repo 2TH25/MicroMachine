e7c34012d145a2997cc6b5386489763b3504ba65
MeloKiki <kiliangaillard25@gmail.com>
2024-05-06 14:04:38 +0200

    ad semi colons

--- plan.js
+++ plan.js
@@ -1,7 +1,7 @@
 class Plan {
     constructor(){
-        this.image = new Image()
-        this.image.src = "ImagePlan.jpg"
+        this.image = new Image();
+        this.image.src = "ImagePlan.jpg";
         this.obstacles = [];
         this.vehicules = [];
         this.longueur = 800;
