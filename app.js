ec72d5f1e9cf3db62315fdbfbc19be4ed61fcb43
2TH25 <mattheo.girardclos@gmail.com>
2024-05-06 02:00:51 +0200

    première rotation

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ function init() {
     context.width = document.getElementById("cvs").width;
     context.height = document.getElementById("cvs").height;
 
+    document.addEventListener("keydown", captureAppuiToucheClavier)
+    document.addEventListener("keyup", captureRelacheToucheClavier)
+
+    rectangle = {x: 0, y: 0, rotation: 0.25}
+    
 
     boucleDeJeu();
 }
@@ -34,14 +39,55 @@ function boucleDeJeu() {
 
 function update(dt) {
     
+
     
 }
 
 function render() {
     context.clearRect(0, 0, context.width, context.height);
-    
     context.fillStyle = "red"
-    context.fillRect(0,0,50,50);
 
+    context.save()
+
+    context.translate(rectangle.x + 25,rectangle.y + 25)
+    context.rotate(Math.PI*rectangle.rotation)
     
+    context.fillRect(-25,-25,80,50);
+
+    context.restore()
+    
+}
+
+function captureAppuiToucheClavier(event) {
+    // pratique pour connaître les keyCode des touches du clavier :
+    //  --> http://www.cambiaresearch.com/articles/15/javascript-key-codes
+    if (event.code == "ArrowUp"){
+        
+    }
+    if (event.code == "ArrowDown"){
+        
+    }
+    if (event.code == "ArrowLeft"){
+        
+    }
+    if (event.code == "ArrowRight"){
+        
+
+    }
+
+}
+
+function captureRelacheToucheClavier(event) {
+    if (event.code == "ArrowUp"){
+        
+    }
+    if (event.code == "ArrowDown"){
+        
+    }
+    if (event.code == "ArrowLeft"){
+        
+    }
+    if (event.code == "ArrowRight"){
+        
+    }
 }
