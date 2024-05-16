e49dab278b17e5428ff0a6f3365d12452f597890
2TH25 <mattheo.girardclos@gmail.com>
2024-05-16 16:37:59 +0200

    rajout constructeur

--- obstacle.js
+++ obstacle.js
@@ -1,5 +1,14 @@
 class Obstacle {
 
+    constructor(posX,posY,longueur,largeur,orientation,source){
+        this.image=new Image()
+        this.image.src = source
+        this.position={x:posX, y: posY};
+        this.longueur=longueur;
+        this.largeur=largeur;
+        this.orientation=orientation;
+    }
+
     constructor(posX,posY,longueur,largeur,orientation){
         this.position={x:posX, y: posY};
         this.longueur=longueur;
--- obstacleRebond.js
+++ obstacleRebond.js
@@ -1,6 +1,6 @@
 class ObstacleRebond extends Obstacle {
-    constructor(){
-        super(5930,1872,100,100,0)
+    constructor(posX,posY,longueur,largeur,orientation){
+        super(posX,posY,longueur,largeur,orientation)
     }
 
 }
